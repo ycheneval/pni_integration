a0ff741a0221adfc56a7e60d57eb1bc793f56a26
Yves Cheneval <yves.cheneval@weforum.org>
2018-05-22 03:32:16 +0200

    Code review

--- src/App/Controller/V1/PniServicesController.php
+++ src/App/Controller/V1/PniServicesController.php
@@ -151,6 +151,7 @@ class PniServicesController {
     if ($player_data['success']) {
       $result = $ph->find($player_data['payload']['id'], $player_data['payload']['current_album_id'], $ph->input->text);
       if ($result['success']) {
+        $wd->watchdog('notice', 'Find results @r', ['@r' => print_r($result, TRUE)]);
         $message = [
           'response_type' => 'ephemeral',
           'text' => "Find results",
