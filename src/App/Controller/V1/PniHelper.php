b2642a53ade2b51242ba1f7b7218a1fdf2df7b8b
Yves Cheneval <yves.cheneval@weforum.org>
2018-05-24 19:10:48 +0200

    Code review

--- src/App/Controller/V1/PniHelper.php
+++ src/App/Controller/V1/PniHelper.php
@@ -931,7 +931,7 @@ class PniHelper {
         'start' => NULL,
         'stop' => NULL,
       ];
-      echo 'Stickers sorted ' . print_r($stickers, TRUE);
+//      echo 'Stickers sorted ' . print_r($stickers, TRUE);
       foreach ($stickers as $a_sticker) {
         if (!is_numeric($a_sticker)) {
           $intervals[] = [
@@ -958,7 +958,7 @@ class PniHelper {
         }
       }
       $intervals[] = $current_interval;
-      echo 'Intervals ' . print_r($intervals, TRUE);
+//      echo 'Intervals ' . print_r($intervals, TRUE);
       // Now we should have all intervals, output them
       $result_array = [];
       foreach ($intervals as $an_interval) {
