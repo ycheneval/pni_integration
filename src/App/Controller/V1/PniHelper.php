2deff0783db7ba068eae100a63571cb16e677db4
Yves Cheneval <yves.cheneval@weforum.org>
2018-05-30 03:56:29 +0200

    Code review

--- src/App/Controller/V1/PniHelper.php
+++ src/App/Controller/V1/PniHelper.php
@@ -1673,7 +1673,7 @@ class PniHelper {
     $this->wd->watchdog('watch', 'For player @p, got watches @w', ['@p' => $player_id, '@w' => print_r($watches, TRUE)]);
 
     $actions = \explode(' ', $params);
-    $cur_action = 'add';
+    $cur_action = (empty($actions) ? 'list' : 'add');
     foreach ($actions as $an_action) {
       switch ($an_action) {
         case 'list':
